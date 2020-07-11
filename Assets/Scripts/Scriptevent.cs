61831200e6daefee1550f1176cab248b3e3931cc
yangtaeyang <taiyang_2@naver.com>
2020-07-12 06:28:27 +0900

    스크립트추가시험
    클릭 이벤트 등록할 함수가 호출이안됨

---
+++ Assets/Scripts/Scriptevent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptevent : MonoBehaviour
+{
+    void OnClickNewGame()
+    {
+        Debug.Log("새 게임");
+    }
+
+    public void OnClickLoad()
+    {
+        Debug.Log("불러오기");
+    }
+
+    public void OnClickOption()
+    {
+        Debug.Log("옵션");
+    }
+
+    public void OnClickQuit()
+    {
+        Debug.Log("게임 끝내기");
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void OnBackButtonEvent()
+    {
+        Debug.Log("뒤로 가기 터치");
+    }
+}
