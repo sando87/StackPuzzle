cd27d1dfb6c2be87da2dc3aae658a6daea19f146
sando87 <tjdwn0406@gmail.com>
2021-05-24 15:06:24 +0900

    added bot leveling

--- Assets/Scripts/Etc/AutoBalancer.cs
+++ Assets/Scripts/Etc/AutoBalancer.cs
@@ -13,6 +13,8 @@ public class AutoBalancerInfo
 
 public class AutoBalancer : MonoBehaviour
 {
+    private int BotLevel = 0;
+
     public static bool AutoBalance
     {
         set
@@ -26,12 +28,13 @@ public class AutoBalancer : MonoBehaviour
 
     IEnumerator DoAutoBalancer()
     {
+        ParseBotLevel();
         InGameManager mgr = null;
         int counter = 0;
-        int counterLimit = 5;
+        int counterLimit = NextSwipeCount();
         while (true)
         {
-            yield return new WaitForSeconds(UnityEngine.Random.Range(0.5f, 2));
+            yield return new WaitForSeconds(NextDelaySec());
 
             if (InGameManager.InstStage.gameObject.activeInHierarchy)
                 mgr = InGameManager.InstStage;
@@ -42,17 +45,33 @@ public class AutoBalancer : MonoBehaviour
 
             if (mgr.IsIdle && mgr.IsAllProductIdle())
             {
-                counter++;
-                if (counter > counterLimit)
+                SwipeDirection dir = SwipeDirection.LEFT;
+                Product nextTarget = FindSkill2(mgr, ref dir);
+                if (nextTarget != null)
                 {
                     counter = 0;
-                    counterLimit = UnityEngine.Random.Range(3, 5);
-                    AutoClickNextProduct(mgr);
+                    mgr.OnSwipe(nextTarget.gameObject, dir);
+                    continue;
                 }
-                else
+
+                nextTarget = FindSkill1(mgr);
+                if(nextTarget != null)
                 {
-                    AutoSwipeNextProduct(mgr);
+                    counter = 0;
+                    mgr.OnClick(nextTarget.gameObject);
+                    continue;
                 }
+
+                if (counter >= counterLimit)
+                {
+                    counter = 0;
+                    counterLimit = NextSwipeCount();
+                    AutoClickNextProduct(mgr);
+                    continue;
+                }
+
+                counter++;
+                AutoSwipeNextProduct(mgr);
             }
         }
     }
@@ -159,5 +178,108 @@ public class AutoBalancer : MonoBehaviour
 
             }
         }
+    }
+
+    private Product FindSkill2(InGameManager mgr, ref SwipeDirection dir)
+    {
+        int mCntX = mgr.CountX;
+        int mCntY = mgr.CountY;
+        int yOff = UnityEngine.Random.Range(0, mCntY);
+        for (int y = 0; y < mCntY; ++y)
+        {
+            int fixedY = (y + yOff) % mCntY;
+            for (int x = 0; x < mCntX; ++x)
+            {
+                Product pro = mgr.Frame(x, fixedY).ChildProduct;
+                if (pro == null || pro.IsLocked || pro.IsChocoBlock || pro.Skill == ProductSkill.Nothing)
+                    continue;
+
+                if (pro.Left() != null && pro.Left().Skill != ProductSkill.Nothing)
+                {
+                    dir = SwipeDirection.LEFT;
+                    return pro;
+                }
+                else if (pro.Right() != null && pro.Right().Skill != ProductSkill.Nothing)
+                {
+                    dir = SwipeDirection.RIGHT;
+                    return pro;
+                }
+                else if (pro.Up() != null && pro.Up().Skill != ProductSkill.Nothing)
+                {
+                    dir = SwipeDirection.UP;
+                    return pro;
+                }
+                else if (pro.Down() != null && pro.Down().Skill != ProductSkill.Nothing)
+                {
+                    dir = SwipeDirection.DOWN;
+                    return pro;
+                }
+            }
+        }
+
+        dir = SwipeDirection.LEFT;
+        return null;
+    }
+    private Product FindSkill1(InGameManager mgr)
+    {
+        int mCntX = mgr.CountX;
+        int mCntY = mgr.CountY;
+        int yOff = UnityEngine.Random.Range(0, mCntY);
+        for (int y = 0; y < mCntY; ++y)
+        {
+            int fixedY = (y + yOff) % mCntY;
+            for (int x = 0; x < mCntX; ++x)
+            {
+                Product pro = mgr.Frame(x, fixedY).ChildProduct;
+                if (pro == null || pro.IsLocked || pro.IsChocoBlock || pro.Skill == ProductSkill.Nothing)
+                    continue;
+
+                return pro;
+            }
+        }
+
+        return null;
+    }
+    private void ParseBotLevel()
+    {
+        string[] strs = UserSetting.UserInfo.deviceName.Split('_');
+        if (strs.Length <= 1)
+            BotLevel = 0;
+        else
+            BotLevel = int.Parse(strs[0]);
+    }
+    private float NextDelaySec()
+    {
+        if(BotLevel < 0)
+            return UnityEngine.Random.Range(1, 3);
+
+        switch (BotLevel)
+        {
+            case 0: return UnityEngine.Random.Range(1, 3);
+            case 1: return UnityEngine.Random.Range(0.7f, 2.5f);
+            case 2: return UnityEngine.Random.Range(0.5f, 2);
+            case 3: return UnityEngine.Random.Range(0.5f, 1.5f);
+            case 4: return UnityEngine.Random.Range(0.5f, 1);
+            case 5: return UnityEngine.Random.Range(0.3f, 0.8f);
+            default: break; 
+        }
+        return UnityEngine.Random.Range(0.3f, 0.8f);
+    }
+    private int NextSwipeCount()
+    {
+        if (BotLevel < 0)
+            return UnityEngine.Random.Range(0, 1);
+
+        switch (BotLevel)
+        {
+            case 0: return UnityEngine.Random.Range(0, 1);
+            case 1: return UnityEngine.Random.Range(1, 2);
+            case 2: return UnityEngine.Random.Range(1, 3);
+            case 3: return UnityEngine.Random.Range(2, 4);
+            case 4: return UnityEngine.Random.Range(2, 5);
+            case 5: return UnityEngine.Random.Range(3, 6);
+            default: break;
+        }
+        return UnityEngine.Random.Range(3, 6);
     }
 }
