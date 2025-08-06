a9f35b546bfa915c35d8b57019e6251d76e3c1a4
이성주 <tjdwn0406@gmail.com>
2025-08-06 18:11:59 +0900

    AWS 주소 세팅

--- Assets/Scripts/Utils/NetClientApp.cs
+++ Assets/Scripts/Utils/NetClientApp.cs
@@ -13,8 +13,8 @@ public class NetClientApp : MonoBehaviour
 
     public static NetClientApp mInst = null;
     public int ServerPort = 9435;
-    private string ServerAddress = "localhost"; //"sjleeserver.iptime.org"; //27.117.158.3
-    //private string ServerAddress = "ec2-3-35-208-197.ap-northeast-2.compute.amazonaws.com"; //3.35.208.197
+    private string ServerAddress = "localhost"; //"sjleeserver.iptime.org";
+    // private string ServerAddress = "ec2-43-203-226-89.ap-northeast-2.compute.amazonaws.com";
 
     TcpClient mSession = null;
     NetworkStream mStream = null;
