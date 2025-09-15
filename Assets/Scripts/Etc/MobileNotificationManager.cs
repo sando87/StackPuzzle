6070095cf641944ed20e74e6f235ee1a7b64334e
이성주 <tjdwn0406@gmail.com>
2025-09-15 15:24:09 +0900

    알람 아이콘 등록

--- Assets/Scripts/Etc/MobileNotificationManager.cs
+++ Assets/Scripts/Etc/MobileNotificationManager.cs
@@ -28,7 +28,7 @@ public class MobileNotificationManager : MonoBehaviour
         var channel = new AndroidNotificationChannel()
         {
             Id = CHANNEL_ID,              // 고유 ID
-            Name = "One-Day Event",            // 설정 화면에 표시될 이름
+            Name = "OneDay Event",            // 설정 화면에 표시될 이름
             Importance = Importance.Default,     // 중요도 (소리/진동 여부)
             Description = "Basic Alarm Channel",      // 설명
         };
@@ -36,10 +36,10 @@ public class MobileNotificationManager : MonoBehaviour
 
         // 2. 알림 생성
         var notification = new AndroidNotification();
-        notification.Title = "게임에 복귀하세요!";
-        notification.Text = "하루가 지났습니다. 다시 접속해 보세요!";
-        notification.SmallIcon = "icon_0";      // Assets/Plugins/Android/Res/drawalbe/icon_0.png 24x24~96x96 작은 아이콘 (필수), 배경 투명, 아이콘 단색
-        notification.LargeIcon = "icon_1";      // Assets/Plugins/Android/Res/drawalbe/icon_1.png 128x128 큰 아이콘 (선택)
+        notification.Title = "Come back to the game!";
+        notification.Text = "A dat has passed.Please try connecting again!";
+        notification.SmallIcon = "NotificationIcon";      // Assets/Plugins/Android/Res/drawable/icon_0.png 24x24~96x96 작은 아이콘 (필수), 배경 투명, 아이콘 단색
+        // notification.LargeIcon = "icon_1";      // Assets/Plugins/Android/Res/drawable/icon_1.png 128x128 큰 아이콘 (선택)
         notification.FireTime = System.DateTime.Now.AddDays(1); // 하루 뒤 실행
 
         // 3. 알림 발송
