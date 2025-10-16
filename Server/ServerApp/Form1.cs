c2f7dc1f74d653200bc20b2ed8b71b7e16a11262
sjlee <tjdwn0406@gmail.com>
2025-10-16 10:17:12 +0900

    극초반 매칭시간 오래 걸리는 문제 개선

--- Server/ServerApp/Form1.cs
+++ Server/ServerApp/Form1.cs
@@ -526,7 +526,7 @@ namespace ServerApp
             if (me.UserInfo.IsSkipFindMatching)
                 return null;
 
-            bool botSkip = me.MatchingTime() < mRandomForBotMatching.Next(100);
+            bool botSkip = me.UserInfo.score < 100 ? false : me.MatchingTime() < mRandomForBotMatching.Next(100);
 
             foreach (SessionUser opp in list)
             {
